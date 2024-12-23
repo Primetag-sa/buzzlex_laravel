82f53726b0521293e6d3bea09ad34319f1da8904
AmrMohamedMesbah <amr.mesbah@creatifyteam.com>
2024-12-23 22:06:39 +0200

    list user orders with filters and resources

---
+++ app/Filters/OrderFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Filters;
+
+use Carbon\Carbon;
+use Filter\Filter;
+
+class OrderFilter extends Filter
+{
+
+    protected function filterType($type)
+    {
+        $this->query->where('type', $type);
+    }
+
+    protected function filterStatus($status)
+    {
+        $this->query->where('status', $status);
+    }
+
+    protected function filterDate($period)
+    {
+        match($period){
+            'today' => $this->query->whereDate('date', today()),
+            'this_week' => $this->query->whereBetween('date', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()]),
+            'this_month' => $this->query->whereBetween('date', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()]),
+            'all_time' => $this->query
+        };
+    }
+}
