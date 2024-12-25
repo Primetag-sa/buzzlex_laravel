f3d30a6fbf849d6702f2b5920abd30a746a558a3
AmrMohamedMesbah <amr.mesbah@creatifyteam.com>
2024-12-25 21:34:52 +0200

    add billboards flag

--- app/Http/Controllers/Api/BillboardController.php
+++ app/Http/Controllers/Api/BillboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BillboardResource;
+use App\Http\Resources\SuccessResource;
 use App\Models\Billboard;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,13 @@ class BillboardController extends Controller
 {
     public function index()
     {
-        return BillboardResource::collection(Billboard::get());
+        $appBillboards = Billboard::where('type', Billboard::APP_TYPE)->get();
+        $userBillboards = Billboard::where('type', Billboard::USER_TYPE)->get();
+        $result = [
+            'app_billboards' => BillboardResource::collection($appBillboards),
+            'user_billboards' => BillboardResource::collection($userBillboards),
+        ];
+        return new SuccessResource($result);
     }
 
     public function show(Billboard $billboard)
