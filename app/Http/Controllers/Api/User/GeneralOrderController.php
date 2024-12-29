02aa61e379d2e14295b1a5b1937ee949b6e325c0
AmrMohamedMesbah <amr.mesbah@creatifyteam.com>
2024-12-29 22:29:44 +0200

    add apis for general orders entity

---
+++ app/Http/Controllers/Api/User/GeneralOrderController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\GeneralOrderRequest;
+use App\Http\Resources\SuccessResource;
+use App\Http\Resources\User\GeneralOrderResource;
+use App\Models\GeneralOrder;
+use Illuminate\Http\Request;
+
+class GeneralOrderController extends Controller
+{
+    public function store(GeneralOrderRequest $request)
+    {
+        $data = $request->validated();
+        $user = auth()->user();
+        $user->generalOrders()->create($data);
+        return new SuccessResource(200, 'Order created successfully');
+    }
+
+    public function index()
+    {
+        $user = auth()->user();
+        $orders = $user->generalOrders()->paginate();
+        return GeneralOrderResource::collection($orders);
+    }
+
+    public function show(GeneralOrder $generalOrder)
+    {
+        return new GeneralOrderResource($generalOrder);
+    }
+
+    public function destroy(GeneralOrder $order)
+    {
+        $order->delete();
+        return new SuccessResource([], 'Order deleted successfully');
+    }
+}
